e8a32e087280bb583b947afd873fea2460f53c17
Igor Lyovkin <49300253+MangriMen@users.noreply.github.com>
2025-01-10 11:22:09 +0700

    fix: loader versions download (#12)
    * fix: loader versions download
    
    * feat: add plugin id to PluginError

--- aether-core/src/api/metadata.rs
+++ aether-core/src/api/metadata.rs
@@ -1,16 +1,15 @@
-use daedalus::modded;
+use daedalus::{minecraft, modded};
 
-use crate::{launcher::download_loaders_manifests, state::LauncherState};
+use crate::state::LauncherState;
+
+pub async fn get_versions_manifest() -> crate::Result<minecraft::VersionManifest> {
+    let state = LauncherState::get().await?;
+    crate::launcher::download_version_manifest(&state, true).await
+}
 
 pub async fn get_loader_versions(loader: &str) -> crate::Result<modded::Manifest> {
     let state = LauncherState::get().await?;
-
-    let loaders = download_loaders_manifests(&state, loader, true).await;
-
-    match loaders {
-        Ok(loaders) => Ok(loaders),
-        Err(_) => {
-            Err(crate::ErrorKind::NoValueFor(format!("{} loader versions", loader)).as_error())
-        }
-    }
+    crate::launcher::download_loaders_manifests(&state, loader, true)
+        .await
+        .map_err(|_| crate::ErrorKind::NoValueFor(format!("{} loader versions", loader)).as_error())
 }
